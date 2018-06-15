19ed5393dfd890714e76dd46440558f43902a552
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-15 15:26:16 +0530

    Add files via upload

---
+++ static vs main.java
@@ -0,0 +1,12 @@
+package JavaTutorial;
+
+public class firstProgram {
+	static {
+		System.out.println("This is static method");
+		// System.exit(0);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("I am the main method");
+	}
+}
