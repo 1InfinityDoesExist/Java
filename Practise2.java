047def19c6ad4bf6546ee8527dbe58ac95aea651
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-27 20:11:16 +0530

    Add files via upload

---
+++ Practise2.java
@@ -0,0 +1,44 @@
+//Differnce between abstract class and interface...
+package practise2;
+
+abstract class abs{
+    abs(){}// 3. Abstract class can have construct...
+    int x = 40;// 2. Abstract class can have instance varibale...
+                // instance variable are final... by default...check this out...next line change the vaiable... say x = 50;
+                // you will see error...
+    abstract void display();
+    void show(){ // 1. abstract class can have method body...(only of non abstract method)
+        System.out.println("Kaise ho");
+    }
+    public static void dekha(){//4 . Abstract class can have static method...
+        System.out.println("ye hey static method");
+    }
+}
+
+//abstract class dushra extend abs{ // 5. Your cannot extent one abstract class....remove comment and see...
+//    abstract void cp();
+//}
+
+abstract class dushra{
+    abstract void cp();
+}
+
+class demo extends abs{// 6. Abstract class does not support multiple inheritance...
+                        // class demo extends abs,dushra{...} you will see error...
+    public void display(){
+        System.out.println("may hu demo class may...");
+    }
+}
+public class Practise2{
+    public static void main(String[] args) {
+       // abs ab = new abs();// You can not have instance of a abstract class...
+        demo d = new demo();
+        d.display();
+        d.show();
+        abs.dekha();//static method can be called directly no need of creating any instance of a class...
+        d.dekha();//calling static method via object...
+        
+        demo.dekha(); // allowed...
+        System.out.println(d.x);
+    }
+}
